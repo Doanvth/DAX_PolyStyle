a668a50821ba405132b475fc63540a9701066081
Mythanh9999 <vothimythanh3472006@gmail.com>
2025-11-11 10:49:47 +0700

    Update UserLayout + Router children + Header Footer

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from "./router";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
+import "bootstrap-icons/font/bootstrap-icons.css"; // Thêm dòng này
 
 const app = createApp(App);
 
--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,15 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import Register from "@/views/auth/Register.vue";
 import LoginView from "../views/auth/Login.vue";
+import UserLayout from "@/layouts/UserLayout.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
-      name: "home",
-      component: HomeView,
+      component: UserLayout,
+      children: [{ path: "", component: HomeView }],
     },
     {
       path: "/register",
